dd85fd015ed83fa8d7428d8894018ee5cf558fa2
PGochman <pedrogoch55@gmail.com>
2023-12-29 19:35:05 -0300

    primeras cosas

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,31 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"my-green-1": "#F3F8F2", 
+				"my-green-2": "#E7F1E4", 
+				"my-green-3": "#DBEAD7", 
+				"my-green-4": "#CFE3CA", 
+				"my-green-5": "#C3DCBC", 
+				"my-green-6": "#B7D5AF", 
+				"my-green-7": "#ABCEA1", 
+				"my-green-8": "#9FC794", 
+				"my-green-9": "#93C087", 
+				"my-green-10": "#87B979", 
+				"my-green-11": "#7BB26C", 
+				"my-green-12": "#6FAB5F", 
+				"my-green-13": "#65A054",
+				"my-green-14": "#5C934D",
+				"my-green-15": "#548646",
+				"my-green-16": "#4C783F", 
+				"my-green-17": "#446B38",
+				"my-green-18": "#3C5E31",
+				"my-green-19": "#34502A",
+				"my-green-20": "#2C4323",
+
+			}
+		},
 	},
 	plugins: [],
 }
