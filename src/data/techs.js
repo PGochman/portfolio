fc192961627c8b599a28c877e5aad3765bdba8f3
PGochman <pedrogoch55@gmail.com>
2023-12-31 18:55:26 -0300

    mas de la mitad

---
+++ src/data/techs.js
@@ -0,0 +1,106 @@
+import AstroIcon from "@/icons/AstroIcon.astro";
+import CSSIcon from "@/icons/CSSIcon.astro";
+import ExpressIcon from "@/icons/ExpressIcon.astro";
+import HTMLIcon from "@/icons/HTMLIcon.astro";
+import JavaScriptIcon from "@/icons/JavaScriptIcon.astro";
+import NextIcon from "@/icons/NextIcon.astro";
+import NodeIcon from "@/icons/NodeIcon.astro";
+import PostgreSQLIcon from "@/icons/PostgreSQLIcon.astro";
+import ReactIcon from "@/icons/ReactIcon.astro";
+import TailwindIcon from "@/icons/TailwindIcon.astro";
+import MercadoPagoIcon from "@/icons/MercadoPagoIcon.astro"
+import FireBaseIcon from "@/icons/FireBaseIcon.astro"
+
+export const html = {
+  name: "HTML",
+  icon: HTMLIcon,
+};
+export const javascript = {
+  name: "JavaScript",
+  icon: JavaScriptIcon,
+};
+export const css = {
+  name: "CSS",
+  icon: CSSIcon,
+};
+export const tailwind = {
+  name: "Tailwind",
+  icon: TailwindIcon,
+};
+export const next = {
+  name: "NextJS",
+  icon: NextIcon,
+};
+export const node = {
+  name: "NodeJS",
+  icon: NodeIcon,
+};
+export const react = {
+  name: "React",
+  icon: ReactIcon,
+};
+export const astro = {
+  name: "Astro",
+  icon: AstroIcon,
+};
+export const express = {
+  name: "Express",
+  icon: ExpressIcon,
+};
+export const postgre = {
+  name: "PostgreSQL",
+  icon: PostgreSQLIcon,
+};
+
+export const mercadoPago = {
+    name: "Mercado Pago",
+    icon: MercadoPagoIcon
+}
+
+export const firebase = {
+    name: "FireBase",
+    icon: FireBaseIcon
+}
+
+export default [
+  {
+    name: "JavaScript",
+    icon: JavaScriptIcon,
+  },
+  {
+    name: "HTML",
+    icon: HTMLIcon,
+  },
+  {
+    name: "CSS",
+    icon: CSSIcon,
+  },
+  {
+    name: "Tailwind",
+    icon: TailwindIcon,
+  },
+  {
+    name: "NextJS",
+    icon: NextIcon,
+  },
+  {
+    name: "React",
+    icon: ReactIcon,
+  },
+  {
+    name: "Astro",
+    icon: AstroIcon,
+  },
+  {
+    name: "NodeJS",
+    icon: NodeIcon,
+  },
+  {
+    name: "Express",
+    icon: ExpressIcon,
+  },
+  {
+    name: "PostgreSQL",
+    icon: PostgreSQLIcon,
+  },
+];
